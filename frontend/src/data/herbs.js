49ebb1911a486be23eedb6508de93159d5aa765c
danielkellydev <danielkellydev@gmail.com>
2023-08-16 13:50:32 +1000

    created initial herbs data file

---
+++ frontend/src/data/herbs.js
@@ -0,0 +1,25 @@
+const herbs = [
+    "Ai ye",
+    "Bai zhi",
+    "Bai zhu",
+    "Ban xia",
+    "Bo he",
+    "Bai shao",
+    "Chai hu",
+    "Chen pi",
+    "Chuan xiong",
+    "Chuan bei mu",
+    "Chi shao",
+    "Chuan lian zi",
+    "Chuan jiao",
+    "Chen xiang",
+    "Chuan mu xiang",
+    "Chen pi",
+    "Chuan bei mu",
+    "Chuan xiong",
+    "Chuan jiao",
+    "Chuan lian zi",
+    "Chen xiang"
+];
+
+export default herbs;
