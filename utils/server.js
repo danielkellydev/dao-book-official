62b467cbe584594b22bce5e9550eaac6ac2c975f
danielkellydev <danielkellydev@gmail.com>
2023-09-05 11:19:01 +1000

    make it better

--- utils/server.js
+++ utils/server.js
@@ -9,20 +9,31 @@ const port = process.env.PORT || 5000;
 
 // Serve static assets
 
-app.use(express.static(path.join(__dirname, "../client/dist")));
+__dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "./client/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+  });
+
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 console.log("Connecting to the database...");
+
 connectToDatabase()
   .then(() => {
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "../client/dist", "index.html"));
-    });
-
+    const port = process.env.PORT || 5000;
     app.listen(port, () => {
       console.log(`Server listening on port ${port}...`);
     });
   })
   .catch((err) => {
-    console.log(`Could not connect to the database. Exiting now...${err}`);
+    console.log(`Could not connect to the database. Exiting now... ${err}`);
     process.exit(1);
   });
