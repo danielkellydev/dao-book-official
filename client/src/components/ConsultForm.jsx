a1ec75bb0ed4148208faa399b77eb7a40c60383c
danielkellydev <danielkellydev@gmail.com>
2023-09-13 14:34:29 +1000

    added tooltip for email function, further developed herb suggestion functionality

--- client/src/components/ConsultForm.jsx
+++ client/src/components/ConsultForm.jsx
@@ -2,7 +2,8 @@ import Button from "../atoms/Button";
 import TextLink from "../atoms/TextLink";
 import MemoFormInput from "../molecules/FormInput";
 import { useState, useEffect, useRef } from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 // Functions
 
@@ -11,53 +12,59 @@ function RequiredLabel() {
 }
 
 function HerbSuggestion({ herb, onSelect }) {
-  console.log(`Rendering suggestion: ${herb.name}`)
   return (
-    <div onClick={() => onSelect(herb)}>
-      {herb.pinyinName} - {herb.latinName} ({herb.abbreviation})
+    <div
+      onClick={() => onSelect(herb)}
+      className="m-1 cursor-pointer rounded-lg border px-1 py-1 text-sm hover:bg-gray-200"
+    >
+      {herb.pinyinName}
     </div>
   );
 }
 
 function HerbInput({ onHerbSelect }) {
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [suggestions, setSuggestions] = useState([]);
 
   useEffect(() => {
-    if (!input.trim()) {
-      setSuggestions([]); // Empty the suggestions if no input
-      return;
+    if (input.length >= 2) {
+      // Ensure at least two characters have been typed
+      fetch("/herbs.json")
+        .then((response) => response.json())
+        .then((data) => {
+          // Filter herb suggestions based on the input value
+          const filteredSuggestions = data.filter(
+            (herb) =>
+              herb.pinyinName.toLowerCase().startsWith(input.toLowerCase()) ||
+              herb.abbreviation.toLowerCase().startsWith(input.toLowerCase())
+          );
+          setSuggestions(filteredSuggestions);
+        })
+        .catch((error) => {
+          console.error("There was an error fetching the herbs list:", error);
+        });
+    } else {
+      setSuggestions([]); // Clear suggestions if less than two characters are typed
     }
-
-    // Fetch the herb list from the herbs.json file
-    fetch('/herbs.json')
-      .then(response => response.json())
-      .then(data => {
-        // Filter herb suggestions based on the input value
-        const filteredSuggestions = data.filter(herb => herb.pinyinName.toLowerCase().includes(input.toLowerCase()));
-        setSuggestions(filteredSuggestions);
-      })
-      .catch(error => {
-        console.error("There was an error fetching the herbs list:", error);
-      });
   }, [input]);
 
   return (
     <div>
-      <input 
-        value={input} 
-        onChange={e => setInput(e.target.value)} 
-        placeholder="Enter herb name (Pinyin)"
+      <input
+        className="mb-1 w-[325px] rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 placeholder:text-[#DFDFDF]"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter herb name (Pinyin) or abbreviation"
       />
-      <div>
-        {suggestions.map(herb => (
-          <HerbSuggestion 
+      <div className="flex flex-wrap">
+        {suggestions.map((herb) => (
+          <HerbSuggestion
             key={herb.pinyinName}
             herb={herb}
             onSelect={(selectedHerb) => {
-              setInput('');
+              setInput("");
               onHerbSelect(selectedHerb);
-            }} 
+            }}
           />
         ))}
       </div>
@@ -76,14 +83,15 @@ function GramInput({ onGramSubmit, grams, setGrams }) {
 
   return (
     <input
+      className="rounded-2xl border-2 border-[#DFDFDF] p-2 px-4 placeholder:text-[#DFDFDF]"
       ref={inputRef}
       value={grams}
       placeholder="Enter grams"
-      onChange={e => setGrams(e.target.value)}
-      onKeyDown={e => {
-        if (e.key === 'Enter' && grams) {
+      onChange={(e) => setGrams(e.target.value)}
+      onKeyDown={(e) => {
+        if (e.key === "Enter" && grams) {
           onGramSubmit(grams);
-          setGrams('');
+          setGrams("");
         }
       }}
     />
@@ -92,29 +100,32 @@ function GramInput({ onGramSubmit, grams, setGrams }) {
 
 function HerbComposer({ onHerbSelection }) {
   const [selectedHerbs, setSelectedHerbs] = useState([]);
-  const [currentHerb, setCurrentHerb] = useState('');
-  const [grams, setGrams] = useState('');
+  const [currentHerb, setCurrentHerb] = useState("");
+  const [grams, setGrams] = useState("");
 
   const handleHerbSelect = (herb) => {
     setCurrentHerb(herb.pinyinName);
   };
 
   const handleGramSubmit = (gramValue) => {
-    onHerbSelection(currentHerb, gramValue); 
-    setSelectedHerbs(prev => [...prev, { name: currentHerb, grams: gramValue }]);
-    setCurrentHerb('');
+    onHerbSelection(currentHerb, gramValue);
+    setSelectedHerbs((prev) => [
+      ...prev,
+      { name: currentHerb, grams: gramValue },
+    ]);
+    setCurrentHerb("");
   };
 
   return (
     <div>
       {currentHerb ? (
-         <GramInput 
-            onGramSubmit={handleGramSubmit} 
-            grams={grams} 
-            setGrams={setGrams}
-         />
+        <GramInput
+          onGramSubmit={handleGramSubmit}
+          grams={grams}
+          setGrams={setGrams}
+        />
       ) : (
-         <HerbInput onHerbSelect={handleHerbSelect} />
+        <HerbInput onHerbSelect={handleHerbSelect} />
       )}
     </div>
   );
@@ -129,15 +140,36 @@ function ConsultForm({
   isDisabled,
 }) {
   const actionButtonText = "Save";
+  const [showTooltip, setShowTooltip] = useState(false);
+
+  const showDelay = useRef(null);
+  const hideDelay = useRef(null);
+
+  const handleMouseOver = () => {
+    if (hideDelay.current) {
+        clearTimeout(hideDelay.current);
+    }
+    showDelay.current = setTimeout(() => {
+        setShowTooltip(true);
+    }, 200); // 200ms delay before tooltip appears
+  }
+
+  const handleMouseOut = () => {
+    if (showDelay.current) {
+        clearTimeout(showDelay.current);
+    }
+    hideDelay.current = setTimeout(() => {
+        setShowTooltip(false);
+    }, 200); // 200ms delay before tooltip disappears
+  }
 
   const handleHerbSelection = (herbName, herbGrams) => {
     const newComposition = `${formData.composition} ${herbName} - ${herbGrams}g; `;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      composition: newComposition
+      composition: newComposition,
     }));
   };
-
 
   return (
     <div className="flex flex-col flex-wrap content-center justify-center gap-4">
@@ -190,7 +222,9 @@ function ConsultForm({
           defaultValue={formData.formulaName}
           isRequired={true}
         ></MemoFormInput>
-        <h2 className="block pb-2 text-xl">Composition <RequiredLabel /></h2>
+        <h2 className="block pb-2 text-xl">
+          Composition <RequiredLabel />
+        </h2>
         <HerbComposer onHerbSelection={handleHerbSelection} />
         <MemoFormInput
           type="textArea"
@@ -216,12 +250,40 @@ function ConsultForm({
           defaultValue={formData.lifestyleAdvice}
           isRequired={true}
         ></MemoFormInput>
-        <MemoFormInput
-          type="checkbox"
-          name="sendEmail"
-          labelText="Email prescription & lifestyle notes"
-          onChange={handleChange}
-        ></MemoFormInput>
+
+        <div className="relative mb-2 flex items-center">
+          {/* Label and Tooltip Icon */}
+          <div className="mr-4 flex items-center">
+            <label className="mr-1">Email prescription & lifestyle notes</label>
+            <FontAwesomeIcon
+              icon={faInfoCircle}
+              className="ml-1 cursor-pointer text-sm" // Adjusted margin and added text-sm for size
+              onMouseOver={handleMouseOver}
+              onMouseOut={handleMouseOut}
+              style={{ fontSize: "1rem" }} // Adjust font size as needed
+            />
+            {showTooltip && (
+              <div
+                className="absolute mt-2 rounded border bg-white p-1 text-xs text-black shadow-lg"
+                style={{
+                  top: "-30px",
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                }} // Adjusted positioning
+              >
+                Checking this box will email the prescription & lifestyle notes
+                to the patient.
+              </div>
+            )}
+          </div>
+
+          {/* Checkbox */}
+          <MemoFormInput
+            type="checkbox"
+            name="sendEmail"
+            onChange={handleChange}
+          />
+        </div>
         {!isSaved ? (
           <Button
             theme="light"
